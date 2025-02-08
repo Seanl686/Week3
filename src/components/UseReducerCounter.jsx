4695e3c5012933e4796b12fc69fae67804c36f7d
Sean Lebida <seanmlebida@cmconnect.cmcc.edu>
2025-02-08 16:33:06 -0500

    Implement useReducer for counter functionality

--- src/components/UseReducerCounter.jsx
+++ src/components/UseReducerCounter.jsx
@@ -1,5 +1,20 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+const initialState = 0;
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'increase':
+            return state + 1;
+        case 'decrease':
+            return state - 1;
+        case 'reset':
+            return initialState;
+        default:
+            return state;
+    }
+};
 
 export const UseReducerCounter = () => {
 

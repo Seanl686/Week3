e4ef509fa3cfdeb855380854b6c2013a771d932c
Sean Lebida <seanmlebida@cmconnect.cmcc.edu>
2025-02-08 16:37:45 -0500

    Refactor UseStateCounter to include count display and button functionality

--- src/components/UseStateCounter.jsx
+++ src/components/UseStateCounter.jsx
@@ -3,14 +3,14 @@ import { useState } from 'react';
 
 export const UseStateCounter = () => {
     const [count, setCount] = useState(0);
-    
+
     return (
         <>
-        <h2>useState counter example</h2>
-        <h1>0</h1>
-        <button>increase</button>
-        <button>decrease</button>
-        <button>reset</button>
+            <h2>useState counter example</h2>
+            <h1>{count}</h1>
+            <button onClick={() => setCount(count + 1)}>increase</button>
+            <button onClick={() => setCount(count - 1)}>decrease</button>
+            <button onClick={() => setCount(0)}>reset</button>
         </>
-    )
-}
+    );
+};
